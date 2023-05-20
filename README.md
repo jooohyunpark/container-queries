5f78e8b4ce835d96d7ba2266dfc77e0070e76e32
jooohyunpark <jhp527@nyu.edu>
2023-05-20 13:12:08 -0400

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# scroll-sequence-demo
+Apple Airpod Pro style interaction using framer-motion and image sequence
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '@/styles/main.scss'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Containter queries test</h1>
+
+      <div style={{ width: '100%', height: '100vh', background: 'orange' }} />
+
+      <div style={{ width: '100%', height: '100vh', background: 'orange' }} />
+    </div>
+  )
+}
+
+export default App
